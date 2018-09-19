c54a8e8c2d3e5f4c0d049f9e4386dc8e03428116
mbojarovic <m.bojarovic@gmail.com>
2018-09-19 22:23:11 +0200

    Working on Pagination

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -12,27 +12,55 @@ class Index extends Controller
     {
         $this->view->langs = $this->lang;
 
-        if(isset($_GET['page']))
-        {
-
-            $offset = ($_GET['page'] - 1) * $_GET['page'];
-            var_dump($offset);
-        }
-
-        $this->view->articles = Article::findMultiAll($offset ?? 0, $_GET['page'] ?? 10);
-        echo $this->view->render(__DIR__ . '/../../templates/index.php');
     }
 
     protected function pagination()
     {
+        $starting_position = 0;
+        $records_per_page = 2;
+        $self = '/../../index/';
+
         if(isset($_GET['page']))
         {
-            $cnt = Article::countAll();
-            echo $cnt;
-
-            $starting_position = ($_GET['page'] - 1) * $records_per_page;
+            $starting_position = ($_GET['page'] -1) * $records_per_page;
         }
-        $query2=$query." limit $starting_position,$records_per_page";
-        return $query2;
-    }
+
+            $this->view->articles = Article::findMultiAll($starting_position, $records_per_page);
+            echo $this->view->render(__DIR__ . '/../../templates/index.php');
+
+            $total_no_of_records = Article::countAll();
+
+            if($total_no_of_records > 0) {
+                ?><tr><td colspan="3">
+
+                    <?php
+                    $total_no_of_pages=ceil($total_no_of_records / $records_per_page);
+                    $current_page = 1;
+
+                    if(isset($_GET['page'])) {
+                        $current_page=$_GET['page'];
+                    }
+
+                    if($current_page != 1) {
+                        $previous = $current_page -1;
+                        echo "<a href='".$self."?page=1'>First</a>&nbsp;&nbsp;";
+                        echo "<a href='".$self."?page=".$previous."'>Previous</a>&nbsp;&nbsp;";
+                    }
+                    for($i=1;$i<=$total_no_of_pages;$i++)
+                    {
+                        if ($i == $current_page) {
+                            echo "<strong><a href='".$self."?page=".$i."' style='color:red;text-decoration:none'>".$i."</a></strong>&nbsp;&nbsp;";
+                        } else {
+                            echo "<a href='".$self."?page=".$i."'>".$i."</a>&nbsp;&nbsp;";
+                        }
+                    }
+
+                    if($current_page != $total_no_of_pages) {
+                        $next=$current_page +1;
+                        echo "<a href='".$self."?page=".$next."'>Next</a>&nbsp;&nbsp;";
+                        echo "<a href='".$self."?page=".$total_no_of_pages."'>Last</a>&nbsp;&nbsp;";
+                    }
+                    ?></td></tr><?php
+            }
+        }
 }
